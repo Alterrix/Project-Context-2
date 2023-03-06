45bca4290a81000ee2534f5f1c653479d04cb277
Merijn Trimbos <merijn.trimbos@gmail.com>
2023-03-06 11:08:35 +0100

    cutscene test

---
+++ Assets/Scripts/Switch/CutsceneSwitch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneSwitch : MonoBehaviour
+{
+    public GameObject player;
+    public Collider playerCollider;
+    public Camera cam;
+    public Animator cameraAnimator;
+
+    private void Start()
+    {
+        playerCollider = player.GetComponent<CapsuleCollider>();
+        cameraAnimator = cam.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other = playerCollider)
+        {
+            Debug.Log("gelukt");
+            cameraAnimator.SetBool(playCutscene,true);
+            
+        }
+        
+    }
+}
